c114729e2a2ba3ed7b41c2a003eb5b1f2a53030f
meiqua <563714976@qq.com>
2018-07-11 00:26:19 +0800

    remove unrelated

--- cxxLCHF/pybind11.cpp
+++
@@ -1,8 +0,0 @@
-#include <pybind11/pybind11.h>
-#include "np2mat/ndarray_converter.h"
-#include "lchf.h"
-namespace py = pybind11;
-
-PYBIND11_MODULE(cxxLCHF_pybind, m) {
-    NDArrayConverter::init_numpy();
-}
